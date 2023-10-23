85a3a9c3d5ea87ad1a99b211982f78a0bcc574e3
argyasatriya <argyasatriyabomantara@gmail.com>
2023-10-23 09:58:43 +0700

    Add project files.

---
+++ JWT-Auth/User.cs
@@ -0,0 +1,9 @@
+﻿namespace JWT_Auth
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
